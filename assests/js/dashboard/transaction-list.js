ec8f52b909546f7a566a24e2bf61e31160a189e8
Ahmed Raza <arahmed212214@gmail.com>
2026-04-16 15:24:11 +0500

    V.22.80 - Ahmed Raza - 16/4/26 3:24 Pm

--- assests/js/dashboard/transaction-list.js
+++ assests/js/dashboard/transaction-list.js
@@ -7,32 +7,32 @@ const transactions = [
 ];
 
 const statusStyles = {
-    'Approved': 'bg-green-50 text-green-500 border-green-100',
-    'Rejected': 'bg-red-50 text-red-400 border-red-100',
-    'Pending': 'bg-yellow-50 text-yellow-500 border-yellow-100'
+    'Approved': 'bg-[#21BB601A] text-[#21BB60] border-green-100',
+    'Rejected': 'bg-[#FAD7E1] text-[#E7366B] border-red-100',
+    'Pending': 'bg-[#F9D40026] text-[#F9D400] border-yellow-100'
 };
 
 const tableBody = document.getElementById('transaction-body');
 
 transactions.forEach(item => {
     const row = document.createElement('tr');
-    row.className = "bg-white border border-gray-100 rounded-lg group hover:shadow-md transition-shadow";
+    row.className = "bg-white border border-[#B09A7026] shadow-sm rounded-lg group hover:shadow-lg transition-shadow";
     
     row.innerHTML = `
-        <td class="px-4 py-4 text-sm text-gray-600 font-medium">${item.id}</td>
-        <td class="px-4 py-4 text-sm text-gray-600">${item.tId}</td>
-        <td class="px-4 py-4 text-sm text-gray-600">${item.method}</td>
-        <td class="px-4 py-4 text-sm text-gray-600">${item.amount}</td>
-        <td class="px-4 py-4 text-sm text-gray-400">${item.date}</td>
+        <td class="px-4 py-4 text-[15px] text-[#000000] font-regular">${item.id}</td>
+        <td class="px-4 py-4 text-[15px] text-[#000000] font-regular">${item.tId}</td>
+        <td class="px-4 py-4 text-[15px] text-[#000000] font-regular">${item.method}</td>
+        <td class="px-4 py-4 text-[15px] text-[#000000] font-regular">${item.amount}</td>
+        <td class="px-4 py-4 text-[15px] text-[#000000] font-regular">${item.date}</td>
         <td class="px-4 py-4 text-center">
-            <span class="px-3 py-1 rounded-full text-xs font-semibold border ${statusStyles[item.status]}">
+            <span class="px-4 py-2 rounded-full text-[14px] font-medium border ${statusStyles[item.status]}">
                 ${item.status}
             </span>
         </td>
-        <td class="px-4 py-4 text-sm text-gray-600">${item.pId}</td>
-        <td class="px-4 py-4 text-center">
-            <button class="text-blue-400 hover:text-blue-600">
-                <i class="fa-regular fa-eye"></i>
+        <td class="px-4 py-4 text-[15px] text-[#000000] font-regular">${item.pId}</td>
+        <td class="px-6 py-4 text-center">
+            <button onclick="viewDetails(${item.id})" class="p-2 bg-[#0077B61A] hover:bg-[#0077B633] rounded-lg transition">
+                <img src="../images/icon-awesome-eye-open.svg" alt="view">
             </button>
         </td>
     `;
